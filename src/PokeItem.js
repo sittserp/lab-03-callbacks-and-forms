29447ab69b55cc062181468d198e906cc3e8ab4f
Perry Sittser <sittserp@gmail.com>
2020-10-28 20:29:58 -0700

    fixed img alt tag compile error

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -5,7 +5,7 @@ export default class PokeItem extends Component {
         return (
             <div className="image-item">
                 <div>{this.props.pokemon}</div>
-                <img className="pokemon-image" src={this.props.url_image} alt='pokemon image' />
+                <img className="pokemon-image" src={this.props.url_image} alt='pokemon' />
                 <div>Type: {this.props.type_1}</div>
                 <div>Attack: {this.props.attack}</div>
                 <div>Defense: {this.props.defense}</div>
